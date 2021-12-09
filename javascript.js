bdf51685d95bb31bf20b7cfb36c6ff082f7fd5cc
Andrew Febre <afebre1027@gmail.com>
2021-12-09 18:51:54 -0500

    local storage

--- javascript.js
+++ javascript.js
@@ -2,10 +2,9 @@ var currentDay = $("#currentDay");
 currentDay.text(moment().format("dddd, MMMM Do YYYY"));
 
 var textArea = $("textarea");
-console.log(textArea);
 
 var currentHour = moment().hour();
-console.log(currentHour);
+//console.log(currentHour);
 
 for (var i = 0; i < textArea.length; i++) {
   // console.log($(textArea[i]).attr("id"))
@@ -19,8 +18,40 @@ for (var i = 0; i < textArea.length; i++) {
     $(textArea[i]).addClass("future");
   }
 }
- var text = $(".saveBtn").text();
 
- localStorage.setItem("texarea", text);
- localStorage.getItem("textarea");
 
+$("button").on("click", function () {
+  console.log($(this).siblings("textarea").val());
+  console.log($(this).siblings("textarea").attr('id'))
+
+  var id = $(this).siblings("textarea").attr('id')
+
+  var saveText = $(this).siblings("textarea").val()
+  localStorage.setItem(id, saveText);
+});
+
+var nine = localStorage.getItem("9");
+console.log(nine);
+for (var i=0; i< textArea.length; i ++){
+   var id = $(textArea[i]).attr("id");
+   var value = localStorage.getItem(id);
+   $(textArea[i]).val(value);
+}
+
+
+
+
+// var text = $("textarea").text();
+
+// var saveTasks = function () {
+//    $("textarea").val();
+//   localStorage.setItem("text", text);
+// };
+// var loadTasks = function () {
+//   localStorage.getItem("text");
+//   $("textarea").val();
+// };
+
+// $("button").on("click", function () {
+//   console.log($(textArea).text());
+// });
